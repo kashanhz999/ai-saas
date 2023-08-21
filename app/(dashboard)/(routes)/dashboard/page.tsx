9bc2a274232cbc13859237b628cc9c854c7fd64b
Kashan Haider <19cs162@jssaten.ac.in>
2023-08-21 18:05:04 +0530

    Added user authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function DashboardPage() {
   return (
-    <Button variant="outline">Button</Button>
+    <div>
+      <p>Dashboard Page (protected)</p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
 
   )
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,26 @@
 import { Button } from "@/components/ui/button";
-
+import Link from "next/link";
 
 export default function LandingPage() {
   return (
-    <p>Landing Page (unprotected)</p>
+    <div>
+      <p>Landing Page (unprotected)</p>
+      <div>
+        <Link href="/sign-in">
+          <Button>
+            Login
+          </Button>
+        </Link>
+      </div>
+      <div>
+        <Link href="/sign-up">
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
+      
+    </div>
 
   )
 }
